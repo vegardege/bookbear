0f4a1b435c5eaefe97a0ea22b41f6d1f8483cf00
Vegard Egeland <vegardegeland@gmail.com>
2024-11-23 14:59:07 +0100

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        Book Bear
+      </main>
+      <footer>
+        Powered by Wikimedia
+      </footer>
+    </div>
+  );
+}
