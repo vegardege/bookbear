0d8e822c2b89abe59a2d1ae61e963497f5ef934f
Vegard Egeland <vegardegeland@gmail.com>
2026-03-10 08:25:42 +0100

    Make hover area of help button bigger

--- src/components/SubTitle.tsx
+++ src/components/SubTitle.tsx
@@ -14,7 +14,9 @@ export default function Heading({ children, action }: HeadingProps) {
 		<div className="flex items-center mx-1 my-6">
 			<h2 className={`${amiri.className} text-3xl flex-1`}>{children}</h2>
 			{action && (
-				<div className="w-14 flex justify-center items-center">{action}</div>
+				<div className="w-14 self-stretch flex justify-center items-center">
+					{action}
+				</div>
 			)}
 		</div>
 	);
